df5f51e01011071f59712e1edb6017c7b3a610f3
constantology <constantology@gmail.com>
2012-09-08 14:01:23 +0100

    added id8.util as another reference to m8

--- src/expose.js
+++ src/expose.js
@@ -6,6 +6,7 @@
 	}, 'w', true );
 
 	util.expose( util, __lib__ );           // store a reference to m8 on id8
+	util.def( __lib__, 'util', util, 'w' ); // store a reference as util as well so we can avoid hard reference in other libs
 
 	anon_list.Class_constructor     = true; // add these two method names to the anonymous function names list
 	anon_list.Class_instance_method = true; // this will give us more clarity when debugging
