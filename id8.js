94acca3e16449f3619b2f3ea969195f7cdbc611b
constantology <constantology@gmail.com>
2013-01-21 14:44:44 +0000

    fix b0rked file concat!

--- id8.js
+++ id8.js
@@ -107,6 +107,35 @@
 
 
 
+/*~  src/vars.js  ~*/
+
+	var __chain__        = '__chain__',
+		__classname__    = '__classname__',
+		__config__       = '__config__',
+		__guid__         = '__guid8__',
+		__method__       = '__method__',
+		__mixins__       = '__mixins__',
+		__name__         = '__name__',
+		__override__     = '__override__',
+		__proto__        = '__proto__',
+		__singleton__    = '__singleton__',
+		__super__        = '__super__',
+		__type__         = '__type__',
+		UNDEF, Name_lc   = Name.toLowerCase(),
+		anon_list        = Function.anon_list,
+		internals        = util.obj(),
+		re_invalid_chars = /[^A-Za-z0-9_\.$<>\[\]\{\}]/g,
+		registered_alias = util.obj(),
+		registered_path  = util.obj(),
+		registered_type  = util.obj(),
+		reserved_props   = [__chain__, __config__, __method__, __override__, __proto__, __type__, 'mixin', 'original', 'parent'].reduce( to_obj, util.obj() );
+
+	internals.empty = { after : null, before : null, mixins : null };
+
+
+
+
+
 /*~  src/lib.define.js  ~*/
 
 util.def( __lib__, 'define', function() {
